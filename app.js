1cc23cfef44726354d43500ba33416c701d32ca4
wanruliao <wan0917ru@gmail.com>
2024-12-09 01:39:28 +0800

    feat: 實作 /restaurants 路由
    串接Google places API，實作 /restaurants 路由。

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const cors = require("cors");
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const restaurantsRouter = require("./routes/restaurants");
+
+require("dotenv").config();
 
 const app = express();
 
-app.use(logger('dev'));
+app.use(cors());
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/restaurants", restaurantsRouter);
 
 module.exports = app;
